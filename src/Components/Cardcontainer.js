e31a41d2cadfc4601f8fa46b12fdd775a34ad335
sarveshkore <sarvesh2k2@gmail.com>
2024-08-02 23:18:30 +0530

    Rendering cards using swiggy data from config files without spread operator

--- src/Components/Cardcontainer.js
+++ src/Components/Cardcontainer.js
@@ -1,116 +1,40 @@
 import Restaurantcard from "./Restaurantcard";
+import { restaurantList } from "../const/config";
+import { IMG_URL } from "../const/config";
 const Cardcontainer=()=>{
-    const restaurantData=[
-        {
-            imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borival",
-            },
-        {
-            imgUrl:"https://tse2.mm.bing.net/th?id=OIP.0l9UIWl8L9HE_N1f8OZQxQHaE8&pid=Api&P=0&h=180",
-            title:"Japanese Wok",
-            starRating:"4.8",
-            deliveryTime:"20-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Ghatkopar",
-        },
-        {
-            imgUrl:"https://tse4.mm.bing.net/th?id=OIP.pUHyxDw89hfkIv3jD6GxgwHaDv&pid=Api&P=0&h=180",
-            title:"Indian Touch",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Vikhroli",
-        },
-        {
-            imgUrl:"https://tse4.mm.bing.net/th?id=OIP.ZM9tzdHo34fFzvN0wg9GxwHaE8&pid=Api&P=0&h=180",
-            title:"Shivraj",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Kurla",
-        },
-        {
-            imgUrl:"https://tse2.mm.bing.net/th?id=OIP.e9lO4u8p0ADHffuzX4XDQQHaFj&pid=Api&P=0&h=180",
-            title:"kabab center",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Mankhurd",
-        },
-        {
-            imgUrl:"https://tse3.mm.bing.net/th?id=OIP.9MogSOYsilcs3FeLbSmUsQHaE8&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borivali",
-        },
-        {
-            imgUrl:"https://tse2.mm.bing.net/th?id=OIP.RyuV9bcGLrFnpO88ZSAR1wHaFj&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borivali",
-        },
-        {
-            imgUrl:"https://tse1.mm.bing.net/th?id=OIP.PFt80Ox4yplK3L_NuLwCKwAAAA&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borivali",
-        },
-        {
-            imgUrl:"https://tse3.mm.bing.net/th?id=OIP.3B5d-t2GEThT20q7kSLcrgHaEK&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borivali",
-        },
-        {
-            imgUrl:"https://tse3.mm.bing.net/th?id=OIP.ON-i9I0UTf3Xqamipfz3CgHaE8&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Borivali",
-        },
-        {
-            imgUrl:"https://tse2.mm.bing.net/th?id=OIP.ybNU8-mQeqBsKoaBuKkWEAHaE8&pid=Api&P=0&h=180",
-            title:"Chinese Wok",
-            starRating:"4.5",
-            deliveryTime:"30-35",
-            cuisines:"Chinese, Tibetian",
-            location:"Kurla",
-        }
-
-    ]
+    console.log("restaurantList",restaurantList);
+    console.log("restaurantList",restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    const restaurants=restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    console.log(restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    
     return(
         <>
         <div className="container d-flex flex-wrap">
             {
-                restaurantData.map((restaurant)=>{
+                restaurants.map((restaurant)=>{
                     return(
                         <Restaurantcard
-                        // imgUrl={restaurant?.imgUrl}
-                        // title={restaurant?.title}
-                        // starRating={restaurant?.starRating}
-                        // deliveryTime={restaurant?.deliveryTime}
-                        // cuisines={restaurant?.cuisines}
-                        // location={restaurant?.location}
-                        {...restaurant}
-                    />
+             imgUrl={IMG_URL+restaurant?.info?.cloudinaryImageId}
+             title={restaurant?.info?.name}
+             starRating={restaurant?.info?.avgRating}
+             deliveryTime={restaurant?.info?.sla?.deliveryTime}
+             cuisines={restaurant?.info?.cuisines.join(", ")}
+             location={restaurant?.info?.areaName}
+            />
                     );
                 })
             }
-            
-           
+
+
+
+            {/* <Restaurantcard
+             imgUrl={IMG_URL+restaurants[0]?.info?.cloudinaryImageId}
+             title={restaurants[0]?.info?.name}
+             starRating={restaurants[0]?.info?.avgRating}
+             deliveryTime={restaurants[0]?.info?.sla?.deliveryTime}
+             cuisines={restaurants[0]?.info?.cuisines.join(", ")}
+             location={restaurants[0]?.info?.areaName}
+                        /> */}
         </div>    
         </>
     );
