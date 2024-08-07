8cabe364c3621cb85a28f2a692c9bdbefcc0909b
sarveshkore <sarvesh2k2@gmail.com>
2024-08-07 20:45:11 +0530

    Implementing API

--- src/Components/Cardcontainer.js
+++ src/Components/Cardcontainer.js
@@ -1,43 +1,46 @@
 import Restaurantcard from "./Restaurantcard";
-import { restaurantList } from "../const/config";
+// import { restaurantList } from "../const/config";
 import { IMG_URL } from "../const/config";
 import { useEffect, useState } from "react";
 const Cardcontainer=()=>{
-    const [restaurantData,setRestaurantData]=useState(restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    // const [restaurantData,setRestaurantData]=useState(restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     // console.log("restaurantList",restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    
-    const[count,setCount]=useState(0);
-    console.log("count",count)
+    const [restaurantData,setRestaurantData]=useState([]);
+    // const[count,setCount]=useState(0);
+    // console.log("count",count)
     // console.log("component is rendered")
     console.log("API is called")
 
     const getRestaurants=async()=>{
-        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        // const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data= await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.07480&lng=72.88560");
         const json=await data.json();
-        console.log(json);
+        console.log(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurantData(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants)
     }
 
     useEffect(()=>{
         getRestaurants();
     },[])
 
+    console.log("Component is rendered");
+    
 
-
-    const filterRestaurants=()=>{
-        const restaurants=restaurantData.filter((restaurant)=>{
-            return(restaurant?.info?.avgRating>=4.5)
-        })
-        setRestaurantData(restaurants)
-        console.log(restaurants);
-        console.log(setRestaurantData)
-    }
+    // const filterRestaurants=()=>{
+    //     const restaurants=restaurantData.filter((restaurant)=>{
+    //         return(restaurant?.info?.avgRating>=4.5)
+    //     })
+    //     setRestaurantData(restaurants)
+    //     console.log(restaurants);
+    //     console.log(setRestaurantData)
+    // }
 
     return(
         <>
-        <div className="container ">
+        {/* <div className="container ">
             <button className="" onClick={filterRestaurants}>Top rated restaurants</button>
-        </div>
-        <div className="container ">
+        </div> */}
+        {/* <div className="container ">
             <h1>Count={count}</h1>
             <button onClick={()=>{
                 if(count<5){
@@ -50,7 +53,9 @@ const Cardcontainer=()=>{
                 // console.log(count)
                 }}}>Decrease</button>
 
-        </div>
+        </div> */}
+        
+        
         <div className="container d-flex flex-wrap justify-content-around">
             {
                     restaurantData.map((restaurant)=>{
